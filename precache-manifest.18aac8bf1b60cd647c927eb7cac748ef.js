d836f8baabbf1782e812d9dfff5dc51a67b4e3ca
Taptso\Taptso <taptso@yahoo.fr>
2019-09-05 04:28:26 +0200

    Updates

---
+++ precache-manifest.18aac8bf1b60cd647c927eb7cac748ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1de18e54cc4a242c9364289d648ac70",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "022f95b860794eeacdfc",
+    "url": "/monsters-rolodex/static/css/main.41a14a29.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "022f95b860794eeacdfc",
+    "url": "/monsters-rolodex/static/js/main.c7fa9846.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
